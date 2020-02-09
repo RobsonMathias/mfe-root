8fc3922737bf1f3dab11e5f00bdf6116e0947e25
Robson Mathias <robsond.mathias@gmail.com>
2020-02-09 09:04:51 -0300

    Fixing script

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
-import './polyfills';
 import { registerApplication, start, getAppStatus } from 'single-spa';
 import * as serviceWorker from './serviceWorker';
+
+declare global {
+    interface Window {
+        System: any;
+    }
+}
+
 
 const legacyApp = (): Promise<any> => window.System.import('@creditas/legacy');
 //
