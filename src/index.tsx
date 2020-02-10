368f1ef924ea4ec80cc5cc5fe22bcc40678e7140
Robson Mathias <robsond.mathias@gmail.com>
2020-02-10 17:58:23 -0300

    Adding style css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { registerApplication, start } from 'single-spa';
 import * as serviceWorker from './serviceWorker';
+import './index.css'
 
 declare global {
     interface Window {
@@ -15,7 +16,8 @@ const simulationApp = (): Promise<any> => window.System.import('@mfe/simulation'
 const simulationV1BetaApp = (): Promise<any> => window.System.import('@mfe/simulation/v0.0.1-beta');
 
 window.client.on(window.client.Event.SDK_READY, () => {
-    // registerApplication('@mfe/authentication', authenticationApp, () => true);
+    registerApplication('@mfe/authentication', authenticationApp, () => true);
+
     registerApplication('@mfe/simulation', simulationApp, () =>
         window.client.getTreatment('mfe-simulation') === 'default' && window.location.pathname.indexOf('/simulation') === 0);
 
