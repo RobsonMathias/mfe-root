07d7a503f8e2e9ad1fc9d12319f9ea09800a9837
Robson Mathias <robsond.mathias@gmail.com>
2020-05-11 09:10:12 -0300

    Change redirect authentication proxy reverse

--- config-overrides.js
+++ config-overrides.js
@@ -12,9 +12,16 @@ module.exports = {
     },
 
     devServer: configFunction => (proxy, allowedHost) => {
+        proxy = {
+            ...proxy,
+            '/authentication/': {
+                target: 'http://localhost:3001',
+            }
+        };
         const config = configFunction(proxy, allowedHost);
         config.disableHostCheck = true;
         config.headers = { 'Access-Control-Allow-Origin': '*' };
+        config.historyApiFallback = true;
         return config;
     },
 
--- src/util/VersionControlUtil.tsx
+++ src/util/VersionControlUtil.tsx
@@ -2,6 +2,6 @@ export const replaceVersion = (treatment: string, appUrl: any) => {
   if (treatment === 'master' || treatment === 'control' || appUrl.indexOf('localhost') > -1) {
     return appUrl;
   } else {
-    return appUrl.replace('://', `://${treatment}--`);
+    return `/${treatment}/${appUrl}`;
   }
 };
